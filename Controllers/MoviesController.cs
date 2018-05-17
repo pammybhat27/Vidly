a503657c43e84631737b694d8b653d7f649cd576
pammybhat27 <31756165+pammybhat27@users.noreply.github.com>
2018-05-17 11:44:00 +1000

    First commit

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() {Name = "Shrek!" };
+
+            var viewModel = new RandomMovieViewModel {
+                Movie = movie
+            };
+            return View(viewModel);
+
+        }
+    }
+}
